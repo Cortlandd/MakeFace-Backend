28c5ecab1f57a3caa91a61294cd41a3ab619c6f2
Cortland Walker <cortlandjwalker@icloud.com>
2020-04-26 21:51:22 -0500

    Added necessary google app engine files and routes for video and image handling

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,50 @@
 from app import app
 import os
 import requests
-from flask import Flask, jsonify
+from flask import jsonify, request
+from google.cloud import storage
+import tempfile
+import cv2
+
+VIDEO_FOLDER_NAME = 'videos'
+PHOTOS_FOLDER_NAME = 'photos'
+CLOUD_STORAGE_BUCKET = 'heroic-gantry-275322.appspot.com'
 
 @app.route('/')
 def index():
     return 'Make Face Backend'
+
+@app.route('/get_files_from_gcs', methods=['POST'])
+def get_files_from_gcs():
+
+    # Get json body
+    json_body = request.get_json()
+    video = json_body['video_name']
+    photo = json_body['photo_name']
+
+    # Create a Cloud Storage client.
+    gcs = storage.Client()
+
+    # Get the bucket that the file will be uploaded to.
+    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+
+    # Fetch files and create temporary file to be used
+    video_blob = bucket.blob(VIDEO_FOLDER_NAME + "/" + video)
+    #video_blob.download_to_filename('tmp/' + video)
+
+    photo_blob = bucket.blob(PHOTOS_FOLDER_NAME + "/" + photo)
+    photo_blob.download_to_filename(photo)
+
+    
+
+    # The public URL can be used to directly access the uploaded file via HTTP.
+    return 'hello'
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
